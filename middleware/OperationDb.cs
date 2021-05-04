0fc0c88d8901f6e7b1807d68dfe3a668f161abd6
yo-fenix <alejandro.iglesias@trifenix.io>
2021-05-04 10:48:38 -0400

    se agrega ejemplo operationDb

---
+++ middleware/OperationDb.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace middleware
+{
+    public class OperationDb<T>
+    {
+        public void Insert(T element) {
+
+        }
+
+        public T GetElement(string id) {
+            throw new Exception();
+        }
+
+        public List<T> GetAll()
+        {
+            throw new Exception();
+        }
+
+
+
+
+
+    }
+}
